6f63b1497b9a21d49d963f5a98ad8584736ec48c
diegojuarezbusiness@gmail.com <diegojuarezbusiness@gmail.com>
2022-08-12 20:25:07 -0400

    Automated scenario for user story DG3-17

--- src/test/java/com/pages/AcademicsSectionPage.java
+++ src/test/java/com/pages/AcademicsSectionPage.java
@@ -4,14 +4,32 @@ import com.web.WebDriverUtils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AcademicsSectionPage {
 
-    public WebElement dynamicSectionXpath(String sectionName) {
+    // SECTIONS LINK
+    @FindBy (xpath = "//a[normalize-space()='Sections']")
+    public WebElement sectionsLink;
 
-        return WebDriverUtils.driver
-                .findElement(By.xpath("//tr/td[contains(text(),'" + sectionName + "')]/following-sibling::td/a[2]"));
+    // SECTIONS TEXT BOX
+    @FindBy (xpath = "//input[@name='section']")
+    public WebElement sectionsTextBox;
+
+    // SECTIONS SAVE BUTTON
+    @FindBy (xpath = "//button[@type='submit'][normalize-space()='Save']")
+    public WebElement sectionsSaveButton;
+
+    /**
+     * USE THIS METHOD TO IDENTIFY THE SECTION NAME IN SECTIONS PAGE TABLE
+     * @param classSection
+     * @return
+     */
+    public WebElement classSectionsDynamicXPATH(String classSection) {
+
+        return WebDriverUtils.driver.findElement(
+                By.xpath("//tr/td[contains(text(),'" + classSection + "')]/following-sibling::td/a[2]"));
 
     }
 
