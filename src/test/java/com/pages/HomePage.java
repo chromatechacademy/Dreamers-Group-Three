a1e5bbbf62f4bd15bb8a371ec95b13c82a8a22f8
Yevgeny22 <yeroshenkousa@gmail.com>
2022-08-14 12:20:03 -0400

    first commit

---
+++ src/test/java/com/pages/HomePage.java
@@ -0,0 +1,45 @@
+package com.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.web.WebDriverUtils;
+
+public class HomePage {
+
+    @FindBy(xpath = "(//*[contains(text(),'Student Information')])[1]")
+    public WebElement studentInformationModule;
+
+    @FindBy(xpath = "//*[contains(text(),' Fees Collection')]")
+    public WebElement feesCollectionModule;
+
+    @FindBy(xpath = "(//*[contains(text(),'Income')])[1]")
+    public WebElement incomeModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Expenses')]")
+    public WebElement expensesModule;
+
+    @FindBy(xpath = "//*[contains(text(), 'Academics')]")
+    public WebElement academicsModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Human Resource')]")
+    public WebElement humanResourceModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Homework')]")
+    public WebElement homeWorkModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Reports')]")
+    public WebElement reportsModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Add Homework')]")
+    public WebElement addHomeWorkModule;
+
+    public HomePage() {
+        PageFactory.initElements(WebDriverUtils.driver, this);
+
+    }
+
+
+
+}
