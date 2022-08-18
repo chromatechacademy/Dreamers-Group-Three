317988797cd3e7c4ca71ed2261ff0fc248cd4a6c
Moek90 <Moekhans990@gmail.com>
2022-08-17 23:32:14 -0400

    ..

--- src/test/java/com/pages/HomePage.java
+++ src/test/java/com/pages/HomePage.java
@@ -8,13 +8,12 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
-
     // Academics module link
-    @FindBy (xpath = "//span[normalize-space()='Academics']")
+    @FindBy(xpath = "//span[normalize-space()='Academics']")
     public WebElement academicsModuleLink;
 
     // Class module link
-    @FindBy (xpath = "//a[normalize-space()='Class']")
+    @FindBy(xpath = "//a[normalize-space()='Class']")
     public WebElement classModuleLink;
 
     @FindBy(xpath = "//a[normalize-space()='Bulk Delete']")
@@ -24,18 +23,53 @@ public class HomePage {
     public WebElement studentInformaion;
 
     @FindBy(xpath = "//a[normalize-space()='Student Categories']")
-    public WebElement studenCategories; 
+    public WebElement studenCategories;
 
+    @FindBy(xpath = "(//*[contains(text(),'Student Information')])[1]")
+    public WebElement studentInformationModule;
 
+    @FindBy(xpath = "(//*[contains(text(),'Student Details')])[2]")
+    public WebElement studentDetailsModule;
 
-    
+    @FindBy(xpath = "//a[normalize-space()='Disabled Students']")
+    public WebElement disabledStudentModule;
 
+    @FindBy(xpath = "//*[contains(text(),' Fees Collection')]")
+    public WebElement feesCollectionModule;
 
+    @FindBy(xpath = "(//*[contains(text(),'Income')])[1]")
+    public WebElement incomeModule;
 
+    @FindBy(xpath = "//*[contains(text(),'Expenses')]")
+    public WebElement expensesModule;
+
+    @FindBy(xpath = "//*[contains(text(), 'Academics')]")
+    public WebElement academicsModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Human Resource')]")
+    public WebElement humanResourceModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Homework')]")
+    public WebElement homeWorkModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Reports')]")
+    public WebElement reportsModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Add Homework')]")
+    public WebElement addHomeWorkModule;
+
+    @FindBy(xpath = "(//*[contains(text(),'Add Expense')])[2]")
+    public WebElement addExpenseModule;
+
+    @FindBy(xpath = "//a[normalize-space()='Search Expense']")
+    public WebElement searchExpenseModule;
+
+    @FindBy(xpath = "//*[contains(text(),'Expense Head')]")
+    public WebElement expenseHeadModule;
 
     // CONSTRUCTOR USING PAGE FACTORY TO INITIALIZE ELEMENTS
-    public HomePage(){
+    public HomePage() {
         PageFactory.initElements(WebDriverUtils.driver, this);
     }
-    
+
 }
--- src/test/java/com/stepDefinitions/addAndDeleteClassStep.java
+++ src/test/java/com/stepDefinitions/addAndDeleteClassStep.java
@@ -1,22 +1,26 @@
 package com.stepDefinitions;
 
 import com.pages.Academics;
+import com.pages.HomePage;
 
 import cucumber.api.java.en.When;
 
 public class addAndDeleteClassStep {
 
+    HomePage homePage = new HomePage();
+    Academics academics = new Academics();
+
     @When("user clicks on {string} module")
     public void user_clicks_on_module(String string) {
 
-        
-        
-
+        homePage.academicsModuleLink.click();
 
     }
 
     @When("user clicks on the {string} page")
     public void user_clicks_on_the_page(String string) {
+
+        academics.ClassTab.click();
 
     }
 
