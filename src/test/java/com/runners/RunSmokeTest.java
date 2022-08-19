195f1b31fd232df225f2d6fb7a0e18ee7504a25e
Serge <casian.sergiu@gmail.com>
2022-08-19 16:46:11 -0400

    Update RunSmokeTest.java

--- src/test/java/com/runners/RunSmokeTest.java
+++ src/test/java/com/runners/RunSmokeTest.java
@@ -17,7 +17,7 @@ import cucumber.api.junit.Cucumber;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"  }, 
 		features = "src/test/java/com/features", 
 		glue = "com.stepDefinitions", 
-		tags = "@DG3-25", 
+		tags = "@Smoke", 
 		dryRun = false, 
 		monochrome = true, 
 		strict = true
